ddce131a91ff26b1621ff8c8253fd855e205032a
Taylor Code <taylorcode.projects@gmail.com>
2023-11-23 10:42:32 -0700

    feat(select): add padding and color

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,20 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const SelectWrapper = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border: 0px;
+  border-radius: 4px;
+  color: ${COLORS.gray700};
+  padding: 8px 12px;
+`;
+
+export function Select({ label, value, onChange, children }) {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectWrapper>
   );
-};
-
-export default Select;
+}
